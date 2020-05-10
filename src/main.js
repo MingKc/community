8da06ea7742541e0f1da92c2790276388f8cd6f0
陈柯明 <1129059298@qq.com>
2020-05-10 14:00:34 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index.js'
+// element-ui
+import './plugins/element.js'
+
+// 导入全局样式表
+import './assets/css/global.css'
+
+// 导入axios
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
